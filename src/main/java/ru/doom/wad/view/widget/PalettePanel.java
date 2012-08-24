6c404aa59dd47c22ef65da13c293986359bbfa79
dmitrysieg <dmitry.sieg@gmail.com>
2012-08-24 23:01:07 +0400

    remove trash

--- src/main/java/ru/doom/wad/view/widget/PalettePanel.java
+++ src/main/java/ru/doom/wad/view/widget/PalettePanel.java
@@ -17,9 +17,8 @@ public class PalettePanel extends JPanel {
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
 
-		BufferedImage image = new BufferedImage(16, 16, BufferedImage.TYPE_INT_RGB);
-		WritableRaster raster = image.getRaster();
-		Graphics imageGraphics = image.getGraphics();
+		final BufferedImage image = new BufferedImage(16, 16, BufferedImage.TYPE_INT_RGB);
+		final WritableRaster raster = image.getRaster();
 
 		final int h = g.getClipBounds().height;
 		final int w = g.getClipBounds().width;
@@ -27,8 +26,6 @@ public class PalettePanel extends JPanel {
 		if (palette != null) {
 			for (int y = 0; y < 16; y++) {
 				for (int x = 0; x < 16; x++) {
-					/*g.setColor(palette.get(y * 16 + x));
-					g.fillRect(x * 16, y * 16, w, h);*/
 					Color color = palette.get(y * 16 + x);
 					raster.setPixel(x, y, new int[]{color.getRed(), color.getGreen(), color.getBlue()});
 				}
