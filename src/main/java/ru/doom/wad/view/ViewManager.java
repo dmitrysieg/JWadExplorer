5cdb153a392a5ac621b1945fde183e62d5517f8d
d.boldyrev <D.Boldyrev@qiwi.ru>
2013-07-02 14:53:10 +0400

    * preserve restored size

--- src/main/java/ru/doom/wad/view/ViewManager.java
+++ src/main/java/ru/doom/wad/view/ViewManager.java
@@ -23,6 +23,7 @@ public class ViewManager {
 	public JFrame createMainFrame() {
 		final JFrame mainFrame = new JFrame();
 		mainFrame.setTitle("WAD Explorer v.1.0");
+		mainFrame.setSize(860, 530);
 		mainFrame.setExtendedState(Frame.MAXIMIZED_BOTH);
 
 		mainFrame.setJMenuBar(createMainMenu());
